216bfab0d38a1296d78b0ab1cd06c35bc5016b51
cdparadise <101418522+cdparadise@users.noreply.github.com>
2023-07-24 10:45:16 -0500

    Add files via upload

---
+++ server.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/portfolio')
+def portfolio_page():
+    return render_template('portfolio.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
